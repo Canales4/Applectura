86c6b20fa8e18d103a1b172727003f7aedca88d1
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-06-19 15:27:01 +0200

    modificando dockerfile del servidor

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
-    host: 'db',
-    port: '3306',
+    host: '165.22.62.165',
+    port: '3307',
     user: 'user290',
     password: 'VHJL_290',
     database: 'app_lecturas'
