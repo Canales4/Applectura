3fc7674b64d2f9df1b92d4577d2a0061c5cb1c78
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-06-20 11:16:11 +0200

    modificando docker-compose

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
-    host: '126.22.62.165',
+    host: '165.22.62.165',
     port: '3307',
     user: 'user_290',
     password: 'VHJL_290',
